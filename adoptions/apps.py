a6574ad6cf8fb0735195323ad0a4cc27f09a863f
nvdhau <nvdhau@gmail.com>
2020-09-27 17:26:37 -0700

    added a new app

---
+++ adoptions/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+# specific settings to this app
+
+class AdoptionsConfig(AppConfig):
+    name = 'adoptions'
---
+++ adoptions/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+
+# Create your views here. (for logic and control flow, handle request and define response)
